637ef261a4dc04ce0197bc19ade92fd45f3c1ff2
최주현 <nostaljoo@choejuhyeon-ui-MacBookAir.local>
2023-01-17 18:05:10 +0900

    실습1. 윈도우 사이즈 표기

--- Quiz1/main.js
+++ Quiz1/main.js
@@ -1,2 +1,13 @@
-const windowScreen = window.screen;
-console.log(windowScreen);
+const scr = document.getElementById("screen");
+const out = document.getElementById("outer");
+const inn = document.getElementById("inner");
+const cli = document.getElementById("client");
+
+window.addEventListener("resize", showWindowsize);
+function showWindowsize() {
+  scr.innerText = `${window.screen.availWidth}, ${window.screen.availHeight}`;
+  out.innerText = `${window.outerWidth}, ${window.outerHeight}`;
+  inn.innerText = `${window.innerWidth}, ${window.innerHeight}`;
+  cli.innerText = `${window.innerWidth}, ${window.innerHeight}`;
+}
+showWindowsize();
